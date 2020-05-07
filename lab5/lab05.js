37c7461a332971919864b32330099ea5afd60155
mmy <2233299790@qq.com>
2020-05-07 19:32:59 +0800

    lab5.js内容丢失

--- lab5/lab05.js
+++ lab5/lab05.js
@@ -9,8 +9,14 @@ let url = document.getElementById("url");
 let url_submit = document.getElementById("url_submit");
 let url_result = document.getElementById("url-result");
 url_submit.addEventListener('click',showWindowHref);
-function showWindowHref(){
-
+function showWindowHref() {
+    var url1 = url.value.indexOf("?");
+    var url2 = url.value.indexOf("=");
+    if (url1 === -1 || url2 === -1) {
+        url_result.value = "格式错误";
+    } else {
+        url_result.value = url.value.slice(url1 + 1, url2);
+    }
 }
 //2. 每隔五秒运行一次函数直到某一整分钟停止，比如从20:55:45运行到20:56:00停止；或者运行10次，先到的为准。从1开始每过五秒，输入框内数值翻倍。初始值为1。
 //注意：你可以在函数 timeTest内部 和 timeTest外部 写代码使得该功能实现。
@@ -18,7 +24,28 @@ function showWindowHref(){
 
 //提示：mul为html中id为"mul"的元素对象，可直接通过mul.value获得其内的输入值。
 let mul = document.getElementById("mul");
+window.addEventListener('load',timeTest)
+mul.value = 1;
+num = 1;
+count = 0;
 function timeTest(){
+    s = setInterval(judge,5000)
+    function judge() {
+        var date = new Date();
+        var second = date.getSeconds();
+        if (second <= 55 && count <= 10) {
+            num = num * 2;
+            count = count + 1;
+            mul.value = num;
+        } else if (second > 55 && count <= 10) {
+            num = num * 2;
+            count = count + 1;
+            mul.value = num;
+            clearInterval(s)
+        }else if(count > 10){
+            clearInterval(s)
+        }
+    }
 }
 //3. 判断输入框most里出现最多的字符，并统计出来。统计出是信息在most_result输入框内以"The most character is:" + index + " times:" + max的形式显示。
 //如果多个出现数量一样则选择一个即可。
@@ -30,5 +57,45 @@ let result = document.getElementById("most-result");
 let most_submit = document.getElementById("most_submit");
 most_submit.addEventListener('click',arrSameStr);
 function arrSameStr(){
-
+    var num = most.value.length;
+    var arr = new Array(num);
+    var i = 0;
+    var k = num-1;
+    var j = 0;
+    var m;
+    var l = 0;
+    var n = 0;
+    var arr1 = new Array();
+    while(i < num){
+        arr[i] = 1;
+        i++;
+    }
+    while(k>0){
+        while(j<arr.length){
+            if(j === k){
+                j = 0;
+                break;
+            }
+            if(most.value.charAt(j) === most.value.charAt(k)){
+                arr[j] = arr[j]+1;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        k--;
+    }
+    m = Math.max.apply(Math,arr);
+    while(l < num){
+        if(arr[l] === m){
+            arr1[n] = l;
+            n++;
+            l++;
+        }else{
+            l++;
+        }
+    }
+    if(arr1.length>0){
+        result.value = "The most character is:" + most.value.charAt(arr1[0]) + " times:" + m;
+    }
 }
